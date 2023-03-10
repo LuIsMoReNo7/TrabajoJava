c0ed1b969b933df543c71aa045d802b24a7136f5
LuIsMoReNo7 <1483853@senati.pe>
2023-03-10 07:39:01 -0500

    probando de otra manera de crear el archivo java

---
+++ dato.java
@@ -0,0 +1,6 @@
+public class dato {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world senatito");
+        System.out.println("Luis");
+    }
+}
